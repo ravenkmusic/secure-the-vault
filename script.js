0fba9f06fe3b04ea3840022f7a9d33566ccc2aaa
Raven Hendricks <ravenkbeats@gmail.com>
2023-11-07 15:57:54 -0500

    added function to retrieve in put from user

--- script.js
+++ script.js
@@ -4,13 +4,31 @@ Step Three: Create alert to invite user to open the vault and display the combin
 Step Four: Initialize three variables to serve as the equations
 to generate the combination
 Step Five: Create text field so that user can enter each number one at a time.
-Step Six: Alert user for each correct entry.
-Step Seven: Create alert for user when combination is entered successfully.
+Step Six: Create functions retrieve input and to alert for user whether or not the combination is entered successfully.
 */
 
 const inviteMessage = alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination: 10 - 40 - 39."); //alerting user to enter combination
 
-const firstNumber = (5 * 2); //equation for first number
-const secondNumber = (8 * 5); // equation for second number
-const thirdNumber = (13 * 3); // equation for third number
+document.getElementById('combination').addEventListener('submit', retrieveInput);
 
+function retrieveInput(){
+    const first = document.getElementById('first').value;
+    const second = document.getElementById('second').value;
+    const third = document.getElementById('third').value;
+    console.log(first);
+    console.log(second);
+    console.log(third);
+
+    openOrShutVault(first, second, third);
+}
+
+function openOrShutVault (first, second, third){
+    const firstNumber = 5 * 2; //equation for first number
+    const secondNumber = 8 * 5; // equation for second number
+    const thirdNumber = 13 * 3; // equation for third number
+        if (first == firstNumber && second == secondNumber && third == thirdNumber){
+            alert("Correct! You've opened the vault!");
+        } else {
+            alert("Not quite. Try again.");
+        }
+}
