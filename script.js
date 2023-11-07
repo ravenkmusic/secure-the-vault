f9f969d876a690603a30e998b9d7ea3d05d625e5
Raven Hendricks <ravenkbeats@gmail.com>
2023-11-07 13:33:10 -0500

    added comments to describe process

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-// Step One: Initialize repository
-//Step Two: Title the page and link javascript file
-//Step Three: Create alert to invite user to open the vault and display the combination.
-//Step Four: Initialize three variables to serve as the equations
-//to generate the combination
-//Step Five: Create text field so that user can enter each number one at a time.
-//Step Six: Alert user for each correct entry.
-//Step Seven: Create alert for user when combination is entered successfully.
+/* Step One: Initialize repository
+Step Two: Title the page and link javascript file
+Step Three: Create alert to invite user to open the vault and display the combination.
+Step Four: Initialize three variables to serve as the equations
+to generate the combination
+Step Five: Create text field so that user can enter each number one at a time.
+Step Six: Alert user for each correct entry.
+Step Seven: Create alert for user when combination is entered successfully.
+*/
 
-const inviteMessage = alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination: 10 - 40 - 39.");
+const inviteMessage = alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination: 10 - 40 - 39."); //alerting user to enter combination
 
 const firstNumber = (5 * 2); //equation for first number
 const secondNumber = (8 * 5); // equation for second number
 const thirdNumber = (13 * 3); // equation for third number
+
